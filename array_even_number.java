112e5d7866336a2a2ab51f15b57923be3b84c296
shruti <shrutinigam59@gmail.com>
2025-02-12 00:09:40 +0530

    first ques commit

---
+++ array_even_number.java
@@ -0,0 +1,18 @@
+
+public class array_even_number {
+
+    public static void main(String[] args) {
+        // int a = 0;
+        int[] Even = {1,2,3,4,5,6,7,8,9,10};
+
+        for (int i = 1; i < Even.length; i++) {
+            if (Even[i] % 2 != 0) {
+                System.out.println(Even[i]);
+
+            }
+
+        }
+            
+        }
+
+    }
