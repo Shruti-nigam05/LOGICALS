bb04957e5ff31ee79041d0924f77a59b7d8f87c9
shruti <shrutinigam59@gmail.com>
2025-03-22 02:00:53 +0530

    STRING

--- java_String.java
+++ java_String.java
@@ -1,3 +1,7 @@
 public class java_String {
-    
+    public static void main(String[] args) {
+        String name = "Shruti";
+        String name1 = new String("Parul");
+        System.out.printf("String as datatype is %S and String as method is %S",name, name1);
+    }
 }
